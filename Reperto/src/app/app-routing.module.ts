cd36f638cc1332fbac674bcb2ed4b643f53c60d8
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-08-11 22:34:30 +0200

    routing

--- Reperto/src/app/app-routing.module.ts
+++ Reperto/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChordComponent } from './chord/chord.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: ChordComponent },
+  { path: 'chord', component: ChordComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
