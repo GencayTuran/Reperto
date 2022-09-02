a52bb3e428d18254b745c2376fb7538ecd3921b7
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-09-02 03:25:40 +0200

    added flex layout module

--- Reperto/src/app/app.module.ts
+++ Reperto/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,8 +11,6 @@ import { SongComponent } from './song/song.component';
 import { RepertoireComponent } from './repertoire/repertoire.component';
 
 import { HttpClientModule } from '@angular/common/http';
-
-import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -25,6 +25,7 @@ import { MaterialModule } from './material.module';
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
